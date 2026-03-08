6db60e6766f40e886b8fa567d90d90ea71facbb8
Emircan Agac <thesadson@gmail.com>
2026-03-08 18:06:05 +0300

    feat: implement initial server setup and add RUST_LOG example to .env.

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -14,7 +14,7 @@ async fn main() {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "voxpery_server=debug,tower_http=debug".into()),
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "voxpery_server=info,tower_http=info".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
